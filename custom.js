adb92ee8daf2b38087e3f5e9294bf0bb74d64515
Svante Richter <svante.richter@gmail.com>
2015-10-25 13:03:21 +0100

    Much improved grid, more docs

--- custom.js
+++ custom.js
@@ -3,8 +3,8 @@ document.querySelector("#menu-icon").addEventListener("click", function () {
     document.querySelector("#menu-icon").classList.toggle("active");
 });
 document.querySelector(".toggle-margin").addEventListener("click", function () {
-    document.querySelector("#grid + button + button + .flex").classList.toggle("margin");
+    document.querySelector("#grid + p + button + button + .flex").classList.toggle("margin");
 });
 document.querySelector(".toggle-padding").addEventListener("click", function () {
-    document.querySelector("#grid + button + button + .flex").classList.toggle("padding");
+    document.querySelector("#grid + p + button + button + .flex").classList.toggle("padding");
 });
