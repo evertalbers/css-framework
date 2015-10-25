44372fedf6035e6e443a680e5818ed02d8e07606
Svante Richter <svante.richter@gmail.com>
2015-10-25 14:23:38 +0100

    tweaking JS

--- custom.js
+++ custom.js
@@ -3,10 +3,10 @@ document.querySelector("#menu-icon").addEventListener("click", function () {
     document.querySelector("#menu-icon").classList.toggle("active");
 });
 document.querySelector(".toggle-margin").addEventListener("click", function () {
-    document.querySelector("#grid + p + button + button + .flex").classList.toggle("margin");
+    document.querySelector(".example:first-of-type").classList.toggle("margin");
 });
 document.querySelector(".toggle-padding").addEventListener("click", function () {
-    document.querySelector("#grid + p + button + button + .flex").classList.toggle("padding");
+    document.querySelector(".example:first-of-type").classList.toggle("padding");
 });
 document.querySelector(".toggle-fixed").addEventListener("click", function () {
     document.querySelector("body").classList.toggle("fixed");
